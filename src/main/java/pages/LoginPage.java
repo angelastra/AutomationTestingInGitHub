7a37740f042a246314e10e83611b81f09e052606
Angel Lastra <aflt210493@gmail.com>
2022-08-22 15:11:13 -0500

    Add LoginPage class

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+
+import resources.BasePage;
+
+public class LoginPage extends BasePage {
+
+	By inputUserName = By.id("userName");
+	By inputPassword = By.id("password");
+	By loginButton = By.id("login");
+	By newUserButton = By.id("newUser");
+	
+	
+	public LoginPage() throws IOException {
+		initializeDriver();
+	}
+	
+	public void clickOnInputUserName() {
+		clickElement(inputUserName);
+	}
+}
